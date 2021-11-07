c11609ad5ba7583aad7e4444e42631fd4784f965
horahoradev <horahora1567@gmail.com>
2021-11-07 08:46:53 -0800

    Gen protos for python

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,11 @@ FROM golang
 RUN apt-get update && \
     apt install -y protobuf-compiler && \
     go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.26 && \
-    go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1
+    go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.1 && \
+    apt-get install -y python3 python3-pip && \
+    python3 -m pip install --upgrade pip && \
+    python3 -m pip install grpcio && \
+    python3 -m pip install grpcio-tools
 
 COPY gen_all.sh /bin/gen_all.sh
 
--- gen_all.sh
+++ gen_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-protoc --go_out=. --go_opt=paths=source_relative     --go-grpc_out=. --go-grpc_opt=paths=source_relative    ./multiplayerservice/protocol/multiplayerservice.proto
+protoc --go_out=. --go_opt=paths=source_relative     --go-grpc_out=. --go-grpc_opt=paths=source_relative ./multiplayerservice/protocol/multiplayerservice.proto
+python3 -m grpc_tools.protoc -I ./multiplayerservice/protocol/ --python_out=./multiplayerservice/protocol/ --grpc_python_out=./multiplayerservice/protocol/ multiplayerservice.proto
